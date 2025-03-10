11e47381f3f3ffbc4c27b898d4341895d26ee83a
Django1111 <163606025+Django1111@users.noreply.github.com>
2025-03-10 12:12:07 -0400

    Add files via upload

---
+++ FT-JS-Tutorial-To-Do-List-MASTER-main/js/app.js
@@ -0,0 +1,88 @@
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+const element= document.getElementById("element");
+const input= document.getElementById("input");
+document.addEventListener("keyup", function(event)){
+    if(event.keyCode == 13){
+        const toDo=input.value;
+        if(toDo){
+            addToDo(toDo);
+        }
+        input.value = "";
+    }
+}
+const list = document.getElementById("list");
+function addToDo(toDo){
+    const text = <li class="item">
+        <i class="co fa fa-circle-thin" job="complete"></i>
+        <p class="text"> ${toDo} </p>
+    </li>
+}
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+const element= document.getElementById("element");
+const input= document.getElementById("input");
+document.addEventListener("keyup", function(event)){
+    if(event.keyCode == 13){
+        const toDo=input.value;
+        if(toDo){
+            addToDo(toDo);
+        }
+        input.value = "";
+    }
+}
+const list = document.getElementById("list");
+function addToDo(toDo){
+    const text = <li class="item">
+        <i class="co fa fa-circle-thin" job="complete"></i>
+        <p class="text"> ${toDo} </p>
+    </li>
+}
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+const element= document.getElementById("element");
+const input= document.getElementById("input");
+document.addEventListener("keyup", function(event)){
+    if(event.keyCode == 13){
+        const toDo=input.value;
+        if(toDo){
+            addToDo(toDo);
+        }
+        input.value = "";
+    }
+}
+const list = document.getElementById("list");
+function addToDo(toDo){
+    const text = <li class="item">
+        <i class="co fa fa-circle-thin" job="complete"></i>
+        <p class="text"> ${toDo} </p>
+    </li>
+}
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+const element= document.getElementById("element");
+const input= document.getElementById("input");
+document.addEventListener("keyup", function(event)){
+    if(event.keyCode == 13){
+        const toDo=input.value;
+        if(toDo){
+            addToDo(toDo);
+        }
+        input.value = "";
+    }
+}
+const list = document.getElementById("list");
+function addToDo(toDo){
+    const text = <li class="item">
+        <i class="co fa fa-circle-thin" job="complete"></i>
+        <p class="text"> ${toDo} </p>
+    </li>
+}
